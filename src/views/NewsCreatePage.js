05827ad82ed2fdb8acbe47d95d553df5632d2c6c
Aida <aidagube@gmail.com>
2023-05-30 20:46:38 +0600

    feat: added news create page

---
+++ src/views/NewsCreatePage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { NewsCreate } from "../components/News/NewsCreate";
+
+export const NewsCreatePage = () => {
+  return (
+    <div>
+      <NewsCreate />
+    </div>
+  );
+};
