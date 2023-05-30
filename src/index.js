85eb3ab42426946097260dfe733e2c299e845dc0
Aida <aidagube@gmail.com>
2023-05-30 16:55:55 +0600

    feat: implement in index.js  Header and Footer

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { BrowserRouter } from "react-router-dom";
+import { Header } from "./components/Header/Header";
+import { Footer } from "./components/Footer/Footer";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <App />
+  <React.StrictMode>
+    <BrowserRouter>
+              <div className='wrapper'>
+                <Header />
+                <App />
+                <Footer />
+              </div>
+    </BrowserRouter>
+  </React.StrictMode>,
 );
